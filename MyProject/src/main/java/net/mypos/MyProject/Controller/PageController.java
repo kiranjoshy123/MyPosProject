53bad935d0082aac8ebf3290f1632c6d7503e399
Kiran Joshy <kiranjoshy123@gmail.com>
2017-12-17 15:19:20 +0530

    Completed the interface theme(not added in page.jsp) and other issues

--- MyProject/src/main/java/net/mypos/MyProject/Controller/PageController.java
+++ MyProject/src/main/java/net/mypos/MyProject/Controller/PageController.java
@@ -11,7 +11,24 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" },method=RequestMethod.GET)
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcom kiran");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickedHome", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = { "/about" })
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickedAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = { "/contact" })
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickedContact", true);
 		return mv;
 	}
 }
